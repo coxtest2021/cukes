56c2afc9a01b0f67d432fa093ddfe5fc47c0b2b8
Juan.Hernandez@coxautoinc.com <Juan.Hernandez@coxautoinc.com>
2021-04-22 11:53:09 -0500

    money will be dispensed by the teller, which will call the cashslot.

--- ch01/atm-example/src/test/java/nicebank/Steps.java
+++ ch01/atm-example/src/test/java/nicebank/Steps.java
@@ -57,9 +57,16 @@ public class Steps {
 
     class Teller
     {
+        private CashSlot cashSlot;
+
+        public Teller(CashSlot cashSlot)
+        {
+            this.cashSlot = cashSlot;
+        }
+
         public void withdrawFrom(Account account, int dollars)
         {
-
+            cashSlot.dispense(dollars);
         }
     }
 

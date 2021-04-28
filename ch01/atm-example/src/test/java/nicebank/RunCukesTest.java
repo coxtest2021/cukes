b76963bb115f3fcd56d009e8e31ca6b778ec7de5
Juan.Hernandez@coxautoinc.com <Juan.Hernandez@coxautoinc.com>
2021-04-28 09:04:51 -0500

    modified runner.

--- ch01/atm-example/src/test/java/nicebank/RunCukesTest.java
+++ ch01/atm-example/src/test/java/nicebank/RunCukesTest.java
@@ -3,7 +3,6 @@ package nicebank;
 import cucumber.api.CucumberOptions;
 import cucumber.api.SnippetType;
 import cucumber.api.junit.Cucumber;
-import cucumber.runtime.snippets.Snippet;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)

417eae5ec34348be73fe2a1259e3f5276cc240ed
Juan.Hernandez@coxautoinc.com <Juan.Hernandez@coxautoinc.com>
2021-04-28 09:02:44 -0500

    added atm interface

--- ch01/atm-example/src/main/java/nicebank/actors/AtmUserInterface.java
+++ ch01/atm-example/src/main/java/nicebank/actors/AtmUserInterface.java
@@ -1,2 +1,29 @@
-package nicebank.actors;public class AtmUserInterface {
+package nicebank.actors;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.events.EventFiringWebDriver;
+
+public class AtmUserInterface implements Teller{
+
+    private CashSlot cashSlot;
+    private EventFiringWebDriver webDriver;
+
+
+    public AtmUserInterface(CashSlot cashSlot)
+    {
+        this.webDriver = new EventFiringWebDriver(new FirefoxDriver());
+        this.cashSlot = cashSlot;
+    }
+
+    public void withdrawFrom(Account account, int dollars) {
+        try
+        {
+            webDriver.get("http://localhost:9988");
+            webDriver.findElement(By.id("Amount")).sendKeys(String.valueOf(dollars));
+            webDriver.findElement(By.id("Withdraw")).click();
+        } finally {
+            webDriver.close();
+        }
+    }
 }
